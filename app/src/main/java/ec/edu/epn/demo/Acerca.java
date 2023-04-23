80490a456b7195d6d46e9c5faac9f4946cf9fc4a
Rodrigo Tufiño <rtufino@ups.edu.ec>
2023-04-23 17:27:50 -0500

    Programado Intents

---
+++ app/src/main/java/ec/edu/epn/demo/Acerca.java
@@ -0,0 +1,14 @@
+package ec.edu.epn.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acerca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca);
+    }
+}
--- app/src/main/java/ec/edu/epn/demo/MainActivity.java
+++ app/src/main/java/ec/edu/epn/demo/MainActivity.java
@@ -2,7 +2,13 @@ package ec.edu.epn.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Crear elementos
+        Button btnImplicito = (Button) findViewById(R.id.btnImplicito);
+        Button btnExplicito = (Button) findViewById(R.id.btnExplicito);
+        // Eventos de los botones
+        btnImplicito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Boton","Intent implicito");
+                implicito();
+            }
+        });
+        btnExplicito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Boton","Intent explicito");
+                explicito();
+            }
+        });
     }
+
+    private void implicito(){
+        Intent intent01 = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://avelectronics.cc"));
+        startActivity(intent01);
+    }
+
+    private void explicito(){
+        Intent intent02 = new Intent(this,
+                Acerca.class);
+        startActivity(intent02);
+    }
+
+
 }
